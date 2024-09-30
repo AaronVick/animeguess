d4a5ffd9c68995cd50438a35e286d0ae617f5880
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-09-30 11:42:03 -0400

    update

--- pages/api/start-game.js
+++ pages/api/start-game.js
@@ -1,7 +1,7 @@
 import { fetchCharacterData, fetchRandomCharacterNames } from './animeService';
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
+  if (req.method !== 'GET' && req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
 
